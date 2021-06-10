1047f16992053ec3af1c2710090d46ff48a96b18
Hari Om Singh <www.hariom03.com@gmail.com>
2021-06-10 12:46:21 +0530

    add keypress event

--- app.js
+++ app.js
@@ -6,46 +6,79 @@ let j = document.querySelector(".j");
 let k = document.querySelector(".k");
 let l = document.querySelector(".l");
 
-let audio1 = new Audio("tom-1.mp3");
-let audio2 = new Audio("tom-2.mp3");
-let audio3 = new Audio("tom-3.mp3");
-let audio4 = new Audio("tom-4.mp3");
-let audio5 = new Audio("kick-bass.mp3");
-let audio6 = new Audio("snare.mp3");
-let audio7 = new Audio("crash.mp3");
+let audio1 = new Audio("sound/tom-1.mp3");
+let audio2 = new Audio("sound/tom-2.mp3");
+let audio3 = new Audio("sound/tom-3.mp3");
+let audio4 = new Audio("sound/tom-4.mp3");
+let audio5 = new Audio("sound/kick-bass.mp3");
+let audio6 = new Audio("sound/snare.mp3");
+let audio7 = new Audio("sound/crash.mp3");
 
+document.addEventListener("keydown", (e) => {
+  console.log(`key: ${e.key}, keycode: ${e.code}`);
+});
+
+const body = document.querySelector("body");
+
+const audioFunction = () => {
+  audio1.play();
+  audio1.currentTime = 0;
+};
+
+body.addEventListener("keydown", (event) => {
+  if (event.key === "a") {
+    audioFunction();
+  } else if (event.key === "s") {
+    audio2.play();
+    audio2.currentTime = 0;
+  } else if (event.key === "d") {
+    audio3.play();
+    audio3.currentTime = 0;
+  } else if (event.key === "f") {
+    audio4.play();
+    audio4.currentTime = 0;
+  } else if (event.key === "j") {
+    audio5.play();
+    audio5.currentTime = 0;
+  } else if (event.key === "k") {
+    audio6.play();
+    audio6.currentTime = 0;
+  } else if (event.key === "l") {
+    audio7.play();
+    audio7.currentTime = 0;
+  }
+});
 a.addEventListener("click", (a) => {
   audio1.play();
-  audio1.currentTime=0;
+  audio1.currentTime = 0;
 });
 
 s.addEventListener("click", () => {
   audio2.play();
-audio2.currentTime=0;
+  audio2.currentTime = 0;
 });
 
 d.addEventListener("click", () => {
   audio3.play();
-audio3.currentTime=0;
+  audio3.currentTime = 0;
 });
 
 f.addEventListener("click", () => {
   audio4.play();
-audio4.currentTime=0;
+  audio4.currentTime = 0;
 });
 
 j.addEventListener("click", () => {
   audio5.play();
-audio5.currentTime=0;
+  audio5.currentTime = 0;
 });
 
 k.addEventListener("click", () => {
   audio6.play();
-audio6.currentTime=0;
+  audio6.currentTime = 0;
 });
 
 l.addEventListener("click", () => {
   audio7.play();
-  audio7.currentTime=0;
+  audio7.currentTime = 0;
 });
-
